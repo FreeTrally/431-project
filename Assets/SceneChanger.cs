e8717e3b128e49943e081250fb2ec543146329a5
FreeTrally <39124135+FreeTrally@users.noreply.github.com>
2020-11-27 04:46:01 +0500

    Aet

---
+++ Assets/SceneChanger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneChanger : MonoBehaviour
+{
+    public void ToGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void ToTask()
+    {
+        SceneManager.LoadScene("TaskScene");
+    }
+    public void ToInv()
+    {
+        SceneManager.LoadScene("InventoryScene");
+    }
+}
