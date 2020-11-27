dfcef7921f03e5308685adc8bd2fe76a8f184dd0
Hennygosa <diemon_1992@mail.ru>
2020-11-28 03:18:57 +0500

    фыв

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collisionInfo) {
+        
+        if (collisionInfo.collider.name == "Ground")
+        {
+            Debug.Log(collisionInfo.collider.name);
+        }
+    }
+}
