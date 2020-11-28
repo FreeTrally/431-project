e9fbd4849480beb2a3146caaa8edd837d6b9c196
Hennygosa <diemon_1992@mail.ru>
2020-11-29 03:54:37 +0500

    29 3:54

---
+++ Assets/StayInside.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StayInside : MonoBehaviour
+{
+    void Update()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -1f, 1f),
+            Mathf.Clamp(transform.position.y, -1f, 1f), transform.position.z);
+    }
+}
