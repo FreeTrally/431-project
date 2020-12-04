cbe2ed05b2e03b7ab353fd08b6df7fa6224f6b3b
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-12-04 20:48:03 +0500

    Меню +
    Добавлено меню, скрипт контроля переходов в меню, заготовки под настройки, таблицу рекордов и выбор уровня

---
+++ Assets/MenuControl.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControl : MonoBehaviour
+{
+    public void ChooseLevelPressed()
+    {
+        SceneManager.LoadScene("ChooseLevel");
+    }
+
+    public void EducationMaterialsPressed()
+    {
+        SceneManager.LoadScene("EducationMaterials");
+        //Открытие ссылки ?
+    }
+
+    public void RecordsPressed()
+    {
+        SceneManager.LoadScene("Records");
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
