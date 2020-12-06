5a969b1c20cfd0b983b16de27467a39316a6069c
PavelLediankin <pavel.lediankin@at.urfu.ru>
2020-12-06 16:27:35 +0500

    AudioSetting !Vk
    Настройки звука (сохранение и извлечение в компонент AudioSource)
    -- Осторожно были локальные проблемы с запуском

---
+++ Assets/Audio/VolumeSettings.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSettings : MonoBehaviour
+{
+    private float masterVolume = 1f;
+    private float audioVolume = 1f;
+    private float musicVolume = 1f;
+
+    void Start()
+    {
+        if (!PlayerPrefs.HasKey("audioVolume"))
+            PlayerPrefs.SetFloat("audioVolume", audioVolume);
+        if (!PlayerPrefs.HasKey("musicVolume"))
+            PlayerPrefs.SetFloat("musicVolume", musicVolume);
+    }
+
+    public void SetAudioVolume(float v)
+    {
+        audioVolume = v * masterVolume;
+        SetVolume();
+    }
+    public void SetMusicVolume(float v)
+    {
+        musicVolume = v * masterVolume;
+        SetVolume();
+    }
+
+    public void SetMasterVolume(float v)
+    {
+        masterVolume = v;
+        audioVolume *= masterVolume;
+        musicVolume *= masterVolume;
+        SetVolume();
+    }
+
+    private void SetVolume()
+    {
+        PlayerPrefs.SetFloat("audioVolume", audioVolume);
+        PlayerPrefs.SetFloat("musicVolume", musicVolume);
+        PlayerPrefs.Save();
+    }
+}
